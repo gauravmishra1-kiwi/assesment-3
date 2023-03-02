20260ecd9dd1e3ae2ca4bb7017bb9c8a0eacf1b4
gauravmishra1-kiwi <gaurav.mishra1@kiwitech.com>
2023-03-02 19:14:42 +0530

    register added

--- model/user.js
+++ model/user.js
@@ -30,10 +30,42 @@ const userSchema = new mongoose.Schema({
    },
    isadmin: {
     type: String,
-    enum: ['Admin','Subadmin','User'],
-    default: 'User'
+    enum: ['admin','subadmin','user'],
+    default: 'user'
 }
 });
+
+userSchema.methods.generateAuthToken=async function(){
+    try {
+    const user=this
+    const token=jwt.sign({_id:user._id.toString() }, "HHHHxzcdsdhfvgdyhsfg")
+        this.tokens=this.tokens.concat({token:token});
+        await this.save(); 
+        console.log(token);
+        return token;
+    } catch (e) {
+        res.send("token genrate error")
+    }
+}
+
+userSchema.statics.findByCredentials=async(email,password)=>{
+    const user=await userdata.findOne({email:email})
+  
+
+    if(!user){
+        throw new Error('unable to login')
+    }
+    
+    const ismatch=await bcrypt.compare(password, user.password)
+    console.log(password);
+    console.log(user.password);
+
+    if(!ismatch){
+        throw new Error('passsword to login')
+    }
+
+    return user
+}
 
 const userdata=new mongoose.model("userdetails",userSchema); 
 
--- server.js
+++ server.js
@@ -4,8 +4,12 @@ require('./db/connection');
 const user=require('./model/user');
 const train=require('./model/tain');
 const station=require('./model/station');
+const userRouter=require('./router/userRouter');
 
-app.use(express.json())
+
+app.use(express.json());
+app.use(userRouter);
+
 
 const port=3000;
 app.listen(port,()=>{
